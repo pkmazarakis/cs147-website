170a576df1ca851462f32a45c1f7766d2c5b9090
pkmazarakis <pkmazarakis@gmail.com>
2022-03-02 18:14:25 -0500

    added team photo

--- src/pages/LandingPages/AboutUs/sections/Team.js
+++ src/pages/LandingPages/AboutUs/sections/Team.js
@@ -1,3 +1,6 @@
+/* eslint-disable global-require */
+/* eslint-disable import/no-unresolved */
+/* eslint-disable jsx-a11y/alt-text */
 /*
 =========================================================
 * Material Kit 2 React - v2.0.0
@@ -15,87 +18,23 @@ Coded by www.creative-tim.com
 
 // @mui material components
 import Container from "@mui/material/Container";
-import Grid from "@mui/material/Grid";
+// import MKTypography from "components/MKTypography";
 
-// Material Kit 2 React components
-import MKBox from "components/MKBox";
-import MKTypography from "components/MKTypography";
+// // Material Kit 2 React examples
+// import HorizontalTeamCard from "examples/Cards/TeamCards/HorizontalTeamCard";
 
-// Material Kit 2 React examples
-import HorizontalTeamCard from "examples/Cards/TeamCards/HorizontalTeamCard";
-
-// Images
-import team1 from "assets/images/PlatonHeadshot.jpg";
-import team2 from "assets/images/TaraHeadshot.jpeg";
-import team3 from "assets/images/AmyHeadshot.jpeg";
-import team4 from "assets/images/AnooshreeHeadshot.jpeg";
+// // Images
+// import team1 from "assets/images/PlatonHeadshot.jpg";
+// import team2 from "assets/images/TaraHeadshot.jpeg";
+// import team3 from "assets/images/AmyHeadshot.jpeg";
+// import team4 from "assets/images/AnooshreeHeadshot.jpeg";
+import TeamPhoto from "../../../../assets/images/Team.jpeg";
 
 function Team() {
   return (
-    <MKBox
-      component="section"
-      variant="gradient"
-      bgColor="dark"
-      position="relative"
-      py={6}
-      px={{ xs: 2, lg: 0 }}
-      mx={-2}
-    >
-      <Container>
-        <Grid container>
-          <Grid item xs={12} md={8} sx={{ mb: 6 }}>
-            <MKTypography variant="h3" color="white">
-              The 5-9 Team
-            </MKTypography>
-            <MKTypography variant="body2" color="white" opacity={0.8}>
-              We are committed to helping post grads be intentional with their time post work
-            </MKTypography>
-          </Grid>
-        </Grid>
-        <Grid container spacing={3}>
-          <Grid item xs={12} lg={6}>
-            <MKBox mb={1}>
-              <HorizontalTeamCard
-                image={team1}
-                name="Platon Mazarakis"
-                position={{ color: "info", label: "Developer" }}
-                description="Symbolic systems major. Wants to bring design and code to bring smiles to peoples faces."
-              />
-            </MKBox>
-          </Grid>
-          <Grid item xs={12} lg={6}>
-            <MKBox mb={1}>
-              <HorizontalTeamCard
-                image={team2}
-                name="Tara Parekh"
-                position={{ color: "info", label: "Designer" }}
-                description="Symbolic systems major. Loves love and wants to build products around it"
-              />
-            </MKBox>
-          </Grid>
-          <Grid item xs={12} lg={6}>
-            <MKBox mb={{ xs: 1, lg: 0 }}>
-              <HorizontalTeamCard
-                image={team3}
-                name="Amy Zhang"
-                position={{ color: "info", label: "Designer" }}
-                description="CS major that loves to build machine learning models to solve intense problems"
-              />
-            </MKBox>
-          </Grid>
-          <Grid item xs={12} lg={6}>
-            <MKBox mb={{ xs: 1, lg: 0 }}>
-              <HorizontalTeamCard
-                image={team4}
-                name="Anooshree Sengupta"
-                position={{ color: "info", label: "Developer" }}
-                description="CS major and Sociology minor. Excited about making post-grad life stress-free!"
-              />
-            </MKBox>
-          </Grid>
-        </Grid>
-      </Container>
-    </MKBox>
+    <Container style={{ alignSelf: "center", alignItems: "center", textAlign: "center" }}>
+      <img src={TeamPhoto} width="90%" style={{ alignSelf: "center" }} />
+    </Container>
   );
 }
 
